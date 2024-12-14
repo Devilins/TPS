ea96be9e57e9a5260068f5d8819281f0f9c61eab
Alexandr <krivovalex4@yandex.ru>
2024-12-14 20:15:46 +0300

    Проект готов к деплою. Dockerfile и docker-compose доделаны. Нет SSH, nginx и джобов

--- Dj_TPS/Dj_TPS/settings.py
+++ Dj_TPS/Dj_TPS/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+# 'ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
