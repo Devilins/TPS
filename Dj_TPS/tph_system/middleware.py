9ca89ca43c702222a30f53c7aa3063ac3ea0ddc7
Alexandr <krivovalex4@yandex.ru>
2025-03-07 02:58:33 +0300

    PROD - Сделано логирование системных ошибок при выключенном дебаге. Также докатаны маленькие изменения, которые уже были на проде.

--- Dj_TPS/tph_system/middleware.py
+++ Dj_TPS/tph_system/middleware.py
@@ -1,8 +1,11 @@
 from threading import local
+import traceback
+import logging
 
 _thread_locals = local()
 
 
+# Middleware для получения текущего пользователя из запроса.
 class CurrentUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -19,3 +22,23 @@ def get_current_user():
     if hasattr(_thread_locals, 'request'):
         return getattr(_thread_locals.request, 'user', None)
     return None
+
+
+logger = logging.getLogger(__name__)
+
+
+# Middleware для логирования всех ошибок. Стандартный способ логирования джанго логирует не все.
+class ErrorLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        logger.error(
+            f"Unhandled exception: {str(exception)}\n"
+            f"Traceback:\n{traceback.format_exc()}"
+        )
+        return None
